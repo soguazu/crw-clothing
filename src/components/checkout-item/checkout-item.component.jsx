e0dc8e141917185dbdea6b0a559584375c4d1e56
dearests <soguazu@gmail.com>
2019-12-10 07:52:29 +0100

    change icon to add and remove item from checkout-item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -19,9 +19,9 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
       </div>
       <span className="name">{name}</span>
       <span className="quantity">
-        <div className="arrow" onClick={() => removeItem(cartItem)}>&#100094;</div>
+        <div className="arrow" onClick={() => removeItem(cartItem)}>&#9001;</div>
         <span className="value">{quantity}</span>
-        <div className="arrow" onClick={() => addItem(cartItem)}>&#100095</div>
+        <div className="arrow" onClick={() => addItem(cartItem)}>&#9002;</div>
       </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</div>
